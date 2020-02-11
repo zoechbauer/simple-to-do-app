3dcabc4032b630102a5a6ba0d19fc46a8caad75e
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-11 13:49:52 +0100

    delete a todo in mongodb

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,5 @@
 document.addEventListener('click', e => {
+  // edit feature
   if (e.target.classList.contains('edit-me')) {
     let userInput = prompt(
       'Enter the desired new name',
@@ -17,6 +18,16 @@ document.addEventListener('click', e => {
           console.log('item was updated');
         })
         .catch(err => console.log(err));
+    }
+  }
+
+  // delete feature
+  if (e.target.classList.contains('delete-me')) {
+    if (confirm('Do you really want to delete this item permanently?')) {
+      axios
+        .post('/delete-item', { id: e.target.getAttribute('data-id') })
+        .then(e.target.parentElement.parentElement.remove())
+        .catch(err => console.log('Delete Error: ', err));
     }
   }
 });
--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', (req, res) => {
             <span class="item-text">${item.text}</span>
             <div>
               <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
           </li>`;
         })
@@ -95,5 +95,14 @@ app.post('/update-item', (req, res) => {
     { _id: new mongodb.ObjectId(req.body.id) },
     { $set: { text: req.body.text } },
     () => res.send('success')
+  );
+});
+
+app.post('/delete-item', (req, res) => {
+  db.collection('items').deleteOne(
+    { _id: new mongodb.ObjectId(req.body.id) },
+    () => {
+      res.send('success');
+    }
   );
 });
